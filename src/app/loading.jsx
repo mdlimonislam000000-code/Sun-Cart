c95a83b7c57849d16e59a71971f67c4f4240295f
mdlimonislam000000 <mdlimonislam000000@gmail.com>
2026-06-19 14:04:27 +0600

    loading fixed

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='flex gap-2 h-[85vh] items-center justify-center'>
-            <p>Global Loading</p>
+            <p> Loading</p>
             <span className="loading loading-ring loading-xl"></span>
         </div>
     );
